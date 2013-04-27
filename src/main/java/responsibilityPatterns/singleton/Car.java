4c9d5c9a3b5ac52b5ce75e831b36cd57fb056818
Harun Yardimci <hyardimci@ebay.com>
2013-04-27 10:50:10 +0300

    singleton

---
+++ src/main/java/responsibilityPatterns/singleton/Car.java
@@ -0,0 +1,10 @@
+package responsibilityPatterns.singleton;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: { "id": "hyardimci", "email":nosqlsolutions@gmail.com"}
+ * Date: 4/27/13
+ * Time: 10:49 AM
+ */
+public class Car {
+}
