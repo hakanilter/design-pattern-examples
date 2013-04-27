651fb28d808d82b338513b043ab66919c0fdc09f
Harun Yardimci <hyardimci@ebay.com>
2013-04-27 10:51:54 +0300

    singleton car class

--- src/main/java/responsibilityPatterns/singleton/Car.java
+++ src/main/java/responsibilityPatterns/singleton/Car.java
@@ -1,10 +1,8 @@
 package responsibilityPatterns.singleton;
 
-/**
- * Created with IntelliJ IDEA.
- * User: { "id": "hyardimci", "email":nosqlsolutions@gmail.com"}
- * Date: 4/27/13
- * Time: 10:49 AM
- */
-public class Car {
+public class Car
+{
+    Car() {
+
+    }
 }
