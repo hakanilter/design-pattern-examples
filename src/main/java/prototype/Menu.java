db3338f4191153c398b7966303b5e43f54f5accc
Hakan İlter <hilter@ebay.com>
2013-04-27 10:11:05 +0300

    copy construct example added to proxy pattern test

--- src/main/java/prototype/Menu.java
+++ src/main/java/prototype/Menu.java
@@ -4,12 +4,16 @@ public class Menu
 {
 	private Tea teaWithOneSugar = new Tea(1);
 	
-	public Tea getTeaWithOneSugar() 
+	public Tea getCloneTeaWithOneSugar() 
 	{
 		try {
 			return (Tea) teaWithOneSugar.clone();
 		} catch (CloneNotSupportedException e) {
 			throw new RuntimeException(e);
 		}
+	}
+	
+	public Tea getCopyTeaWithOneSugar() {
+		return new Tea(teaWithOneSugar);
 	}
 }
--- src/main/java/prototype/Tea.java
+++ src/main/java/prototype/Tea.java
@@ -1,11 +1,21 @@
 package prototype;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tea implements Cloneable
 {
 	private int sugarCount;
+	private List<String> list = new ArrayList<String>();
 	
+	// constructor
 	public Tea(int sugarCount) {
 		this.sugarCount = sugarCount;
+	}
+	
+	// copy constructor
+	public Tea(Tea tea) {
+		this.sugarCount = tea.sugarCount;
 	}
 	
 	public int getSugarCount() {
@@ -14,6 +24,10 @@ public class Tea implements Cloneable
 	
 	public void setSugarCount(int sugarCount) {
 		this.sugarCount = sugarCount;
+	}
+	
+	public List<String> getList() {
+		return list;
 	}
 	
 	@Override
