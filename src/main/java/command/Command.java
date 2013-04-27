dfd07a3408639c59ad92f3fadc4542b81007f2c0
Hakan İlter <hilter@ebay.com>
2013-04-27 11:33:32 +0300

    command pattern example

---
+++ src/main/java/command/Command.java
@@ -0,0 +1,7 @@
+package command;
+
+public interface Command 
+{
+	String getName();
+	void execute();
+}
---
+++ src/main/java/command/OrderDrinkCommand.java
@@ -0,0 +1,14 @@
+package command;
+
+public class OrderDrinkCommand implements Command 
+{
+	@Override
+	public String getName() {
+		return "Order Drink";
+	}
+
+	@Override
+	public void execute() {
+		System.out.println("drink ordered");
+	}
+}
